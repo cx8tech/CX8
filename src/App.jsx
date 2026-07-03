03852daa84d760f3a2a36291ab2147d2780c1d88
kexyaaaaa <keya.gurjal@gmail.com>
2026-07-03 15:11:21 +0530

    Hide footer on tool viewer pages so iframe fills full height

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
@@ -6,24 +6,33 @@ import Tools from './pages/Tools'
 import ToolViewer from './pages/ToolViewer'
 import ComingSoon from './pages/ComingSoon'
 
+function AppLayout() {
+  const { pathname } = useLocation()
+  const isToolViewer = /^\/tools\/.+/.test(pathname)
+
+  return (
+    <div className="page-wrap">
+      <Header />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tools" element={<Tools />} />
+          <Route path="/tools/:toolId" element={<ToolViewer />} />
+          <Route path="/suppliers" element={<ComingSoon page="Supplier Directory" />} />
+          <Route path="/community" element={<ComingSoon page="Community" />} />
+          <Route path="/courses" element={<ComingSoon page="Courses" />} />
+          <Route path="/resources" element={<ComingSoon page="Resources" />} />
+        </Routes>
+      </main>
+      {!isToolViewer && <Footer />}
+    </div>
+  )
+}
+
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="page-wrap">
-        <Header />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/tools" element={<Tools />} />
-            <Route path="/tools/:toolId" element={<ToolViewer />} />
-            <Route path="/suppliers" element={<ComingSoon page="Supplier Directory" />} />
-            <Route path="/community" element={<ComingSoon page="Community" />} />
-            <Route path="/courses" element={<ComingSoon page="Courses" />} />
-            <Route path="/resources" element={<ComingSoon page="Resources" />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <AppLayout />
     </BrowserRouter>
   )
 }
